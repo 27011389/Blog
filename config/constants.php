186af18ddeab31961b6b80c610d67e638152aa6e
Mark Pepere <83042083+27011389@users.noreply.github.com>
2022-11-16 11:43:41 +0700

    Updated creds

--- config/constants.php
+++ config/constants.php
@@ -2,6 +2,8 @@
 session_start();
 //define('ROOT_URL', 'http://localhost/blog/');
 define('DB_HOST', 'us-cdbr-east-06.cleardb.net');
-define('DB_USER', 'b4208b7903609d');
-define('DB_PASS', '58f63386');
+//define('DB_USER', 'b4208b7903609d');
+define('DB_USER', 'egator');
+//define('DB_PASS', '58f63386');
+define('DB_PASS', 'admin1234');
 define('DB_NAME', 'heroku_e129ab20bdb1a9c');
